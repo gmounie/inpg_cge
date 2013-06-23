36abec568d63dd30faa4209a0aadd84ba5034036
Grégory <Gregory.Mounie@imag.fr>
2013-06-23 13:31:54 +0200

    situation 2013

--- ScriptsR/SituationEmplois/situation_emplois.R
+++ ScriptsR/SituationEmplois/situation_emplois.R
@@ -5,6 +5,7 @@ data2013 = data2013[data2013$AnneeEnquete == 2013,]
 taux_emplois = data.frame(situation=data2013$ActiviteActuelle, activitesOLD=data2013$ActiviteActuelleV2010, promo=data2013$PromoEnquete2013, poids=100, nb=1)
 taux_emplois[taux_emplois$promo == 2011,"nb"] = length(taux_emplois[taux_emplois$promo == 2011,"situation"])
 taux_emplois[taux_emplois$promo == 2012,"nb"] = length(taux_emplois[taux_emplois$promo == 2012,"situation"])
+taux_emplois[,"poids"] = 100/taux_emplois[,"nb"]
 taux_emplois$situation = relevel(taux_emplois$situation, "En recherche d'emploi")
 taux_emplois$situation = relevel(taux_emplois$situation, "Volontariat")
 taux_emplois$situation = relevel(taux_emplois$situation, "En thèse")
@@ -13,9 +14,15 @@ taux_emplois$situation = relevel(taux_emplois$situation, "En activité professio
 taux_emplois$situation = relevel(taux_emplois$situation, "")
 svg("../../Output/ensimag_2013_situation.svg")
 library(ggplot2)
-p = ggplot(data=taux_emplois, aes(x=as.factor(promo), fill=situation, weigth=poids/nb), colour=black)  + geom_bar()
-p + scale_fill_manual(values=c("blue","green","SlateBlue4","chartreuse4","red","yellow", "orange")) + opts(title="Situation des diplômés début 2O13, 6 et 18 mois") + xlab("Promo Ensimag") + ylab("Pourcentage")
+p = ggplot(data=taux_emplois, aes(x=as.factor(promo), fill=situation, weight=poids), colour=black)  + geom_bar()
+p + scale_fill_manual(values=c("blue","green","SlateBlue4","chartreuse4","red","yellow", "orange")) + opts(title="Situation des diplômés début 2O13, à 6 et 18 mois") + xlab("Promo Ensimag") + ylab("Pourcentage")
 dev.off()   
+
+
+
+
+
+
 
 
 
