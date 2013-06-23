d838cbb48cfd92b27dfb2fc1b3aafd2f8ecd4689
Grégory <Gregory.Mounie@imag.fr>
2013-06-23 15:42:22 +0200

    repondants

--- ScriptsR/Repondants/repondants.R
+++ ScriptsR/Repondants/repondants.R
@@ -1,4 +1,4 @@
-source("script_data.R")
+source("../DataReader/dataReader.R")
 
 
 repondants6_2012 = length( data2013$AnneeEnquete[ data2013$AnneeEnquete == 2013 & data2013$AnneeDiplome == 2012 & data2013$ActiviteActuelle != ""])
@@ -13,9 +13,9 @@ repondants_CGE13 = data.frame(annee=2013, promo=c(2012, 2011),
 
 
 library(ggplot2)
-p = ggplot(data = repondants_CGE13, aes(x=as.factor(promo), weight=100*repondants/taillepromo)) + geom_bar()
-p +  opts(title="Pourcentage de répondants, début 2O13, promo à 6 et 18 mois") + xlab("Promo Ensimag") + ylab("Pourcentage")  + facet_grid(. ~ annee) + coord_flip()
-
+p = ggplot(data = repondants_CGE13, aes(x=as.factor(promo), fill=as.factor(promo), weight=100*repondants/taillepromo)) + geom_bar()+ scale_fill_manual(values=c("blue","blue"))
+p +  opts(title="Pourcentage de répondants, début 2O13, promo à 6 et 18 mois") + xlab("Promo Ensimag") + ylab("Pourcentage")  + facet_grid(. ~ annee) + coord_flip(ylim=c(0,100))
+ggsave("../../Output/repondants.svg")
 
 
 
