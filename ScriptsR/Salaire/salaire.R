5a6115d0595c38cb57e83e537881c04a203af77e
Grégory Mounié <Gregory.Mounie@imag.fr>
2013-10-09 16:30:06 +0200

    salaires

--- ScriptsR/Salaire/salaire.R
+++ ScriptsR/Salaire/salaire.R
@@ -1,4 +1,4 @@
-source("script_data.R")
+source("../DataReader/dataReader.R")
 
 ## Donne le salaire moyen et le salaire médian ?
 salaireBrut2013 = as.data.frame(data2013[ data2013$AnneeEnquete == 2013, c("SalaireBrutAnnuelAvecPrimes", "SalaireBrutAnnuelHorsPrimes", "Sexe", "AnneeDiplome", "AnneeEnquete", "RevenuBrutAnnuelAvecPrimesThesard", "RevenuBrutAnnuelHorsPrimesThesard", "ActiviteActuelle", "LieuTravailDetailEnquete2012", "LieuLaboDoctoratEnquete2012")])
@@ -34,12 +34,12 @@ summary(salaire$SalaireAvecPrime[salaire$AnneeEnquete == 2012 & salaire$AnneeDip
 
 p = ggplot(data=salaire, aes(x=AnneeDiplome, y=SalaireAvecPrime, ymin=0)) + geom_jitter(aes(alpha=0.01)) + geom_boxplot(outlier.size=0)
 p + facet_grid(AnneeEnquete ~ AnneeDiplome) + opts(title="Salaire Brut (+ primes) par année de promotion et année de l'enquête")
-ggsave("ensimag_salaire_total.svg", width=2*par("din")[1])
+ggsave("../../Output/ensimag_salaire_total.pdf", width=2*par("din")[1])
 p + facet_grid(AnneeEnquete ~ AnneeDiplome + Sexe) + opts(title="Salaire Brut (+ primes) par année de promotion, sexe et année de l'enquête")
-ggsave("ensimag_salaire_sexe.svg", width=2*par("din")[1])
+ggsave("../../Output/ensimag_salaire_sexe.pdf", width=2*par("din")[1])
 p + facet_grid(AnneeEnquete ~ AnneeDiplome + ActiviteActuelle) + opts(title="Salaire Brut (+ primes) par année de promotion, activité et année de l'enquête")
-ggsave("ensimag_salaire_activite.svg", width=2*par("din")[1])
+ggsave("../../Output/ensimag_salaire_activite.pdf", width=2*par("din")[1])
 p + facet_grid(AnneeEnquete ~ AnneeDiplome + lieu) + opts(title="Salaire Brut (+ primes) par année de promotion, lieu de travail et année de l'enquête")
-ggsave("ensimag_salaire_lieu.svg", width=2.2*par("din")[1])
+ggsave("../../Output/ensimag_salaire_lieu.pdf", width=2.2*par("din")[1])
 
 
