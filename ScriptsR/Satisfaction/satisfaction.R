38578ea361c4537fa47177d4e239f83dae6ea3cc
Grégory Mounié <Gregory.Mounie@imag.fr>
2013-12-31 13:14:26 +0100

    satisfaction en svg

--- ScriptsR/Satisfaction/satisfaction.R
+++ ScriptsR/Satisfaction/satisfaction.R
@@ -10,8 +10,6 @@ data2013$RessentiFormationINPG = relevel(data2013$RessentiFormationINPG, "")
 data2013$RessentiFormationINPG = relevel(data2013$RessentiFormationINPG, "Insatisfaisante")
 data2013$RessentiFormationINPG = relevel(data2013$RessentiFormationINPG, "Très insatisfaisante")
 
-
-
-p = ggplot(data2013, aes(x= SatisfactionEmploiActuel, y=RessentiFormationINPG, color=FiliereFormation)) + geom_point() + geom_jitter(width=.3,height=.1) + scale_color_brewer(palette="Set1")
+p = ggplot(data2013, aes(x= SatisfactionEmploiActuel, y=RessentiFormationINPG, color=FiliereFormation)) + geom_point() + geom_jitter(width=.2,height=0.25) + scale_color_brewer(palette="Set1") + labs("Qualité de l'emploi (de 1 à 5) versus le ressenti de la formation") + xlab("Qualité de l'emploi (de 1 à 5)") + ylab("Ressenti de la formation")
 p
-ggsave("../../Output/satisfaction.pdf", width=2*par("din")[1])
+ggsave("../../Output/satisfaction.svg", width=2*par("din")[1])
