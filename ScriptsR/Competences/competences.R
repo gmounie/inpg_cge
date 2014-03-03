eca3a159e2c8f1c94bde67704061de3e72cf9248
Grégory Mounié <Gregory.Mounie@imag.fr>
2014-03-03 14:53:34 +0100

    qq changement dans l'affichage

--- ScriptsR/Competences/competences.R
+++ ScriptsR/Competences/competences.R
@@ -9,10 +9,10 @@ library(stringr)
 library(plyr)
 library(ggplot2)
 a = str_split(data2013$ActivitesTechniquesFonctionINPG,";")
-
+a = a[a[] != ""]
 taille=length(a[a[] != ""])
 b = unlist(a)
-c = as.factor(str_replace_all(b, " ", ""))
+c = as.factor(str_replace_all(b, "^ | $", ""))
 data = data.frame(compétence=c, poids=1/taille)
 p = ggplot(data, aes(x=compétence, weight=poids))+ geom_bar(fill="lightgreen", colour="darkgreen") + coord_flip() + opts(title="Compétences techniques") + xlab("") + ylab("Pourcentage") 
 p 
