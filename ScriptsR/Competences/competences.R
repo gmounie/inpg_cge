ad243ff694e2a6373948c198c45d3c02eae0b3a3
Grégory Mounié <Gregory.Mounie@imag.fr>
2014-03-03 14:33:01 +0100

    qq différences

--- ScriptsR/Competences/competences.R
+++ ScriptsR/Competences/competences.R
@@ -5,5 +5,15 @@
 
 source("../DataReader/dataReader.R")
 
-data2013$ActivitesTechniquesFonctionINPG
+library(stringr)
+library(plyr)
+library(ggplot2)
+a = str_split(data2013$ActivitesTechniquesFonctionINPG,";")
 
+taille=length(a[a[] != ""])
+b = unlist(a)
+c = as.factor(str_replace_all(b, " ", ""))
+data = data.frame(compétence=c, poids=1/taille)
+p = ggplot(data, aes(x=compétence, weight=poids))+ geom_bar(fill="lightgreen", colour="darkgreen") + coord_flip() + opts(title="Compétences techniques") + xlab("") + ylab("Pourcentage") 
+p 
+ggsave("../../Output/competence.pdf", width=2*par("din")[1])
