941b862465d79c9fbf9fc5512d1c74b5f6413d8c
Grégory Mounié <Gregory.Mounie@imag.fr>
2014-03-26 09:17:19 +0100

    chemin data 2014 au 26 mars

--- ScriptsR/DataReader/dataReader.R
+++ ScriptsR/DataReader/dataReader.R
@@ -1,4 +1,4 @@
-data2014 = read.csv("../../../ObservatoirePourLEmploi.git/CGE2014/_ensimag2014_547.csv", sep =";", header=T, dec=",")
+data2014 = read.csv("../../../ObservatoirePourLEmploi.git/CGE2014/_ensimag2014_125.csv", sep =";", header=T, dec=",")
 data2013 = read.csv("../../../ObservatoirePourLEmploi.git/CGE2013/_ensimag2013-filtree-18-avr-2013.csv", sep =";", header=T, dec=",")
 data2012 = read.csv("../../../ObservatoirePourLEmploi.git/CGE2012/_cge2012-ensimag_final_26mars.csv", sep =";", header=T, dec=",")
 data2011 = read.csv("../../../ObservatoirePourLEmploi.git/CGE2011/cge-ensimag-2011_SPHINX_7avril2011.csv", sep =";", header=T, dec=",");
