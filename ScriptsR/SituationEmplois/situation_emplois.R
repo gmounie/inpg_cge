04d6dc111f631adeec6f72feb2eaf8fe1d0af3b5
Grégory Mounié <Gregory.Mounie@imag.fr>
2014-04-11 14:37:25 +0200

    situation a 6 et 18 mois sur les 5 dernière années

--- ScriptsR/SituationEmplois/situation_emplois.R
+++ ScriptsR/SituationEmplois/situation_emplois.R
@@ -61,6 +61,93 @@ ggsave("../../Output/ensimag_2012_situation.svg")
 ggsave("../../Output/ensimag_2012_situation.pdf")
 
 
+# promo 2008-2012 a 18 mois
+
+sit2008 = as.character(data2010_2008$situation.actuelle)
+sit2008[data2010_2008$Type.d.études == "Doctorat"] = "En thèse"
+sit2008[sit2008 == "A - Vous êtes en activité professionnelle"] = "En activité professionnelle"
+sit2008[sit2008 == "C - Vous êtes en Volontariat (VIE ou VIA)"] = "Volontariat"
+sit2008[sit2008 == "B - Vous êtes en recherche d'emploi"] = "En recherche d'emploi"
+sit2008[sit2008 == "D - Vous êtes en poursuite d'études"] = "En poursuite d'études (Hors thèse)"
+promo2008= sit2008
+promo2008[]= "2008"
+
+situationALL= factor( c(as.character(sit2008), as.character(data2011$ActiviteActuelle[data2011$Promo == 2009]) , as.character(data2012$ActiviteActuelle[data2012$AnneeDiplome == 2010]), as.character(data2013$ActiviteActuelle[data2013$AnneeDiplome == 2011]), as.character(data2014$ActiviteActuelle[data2014$AnneeDiplome == 2012]) ))
+length(situationALL)
+promoALL= factor(c( as.character(promo2008), as.character(data2011$Promo[data2011$Promo == 2009]) , as.character(data2012$AnneeDiplome[data2012$AnneeDiplome == 2010]), as.character(data2013$AnneeDiplome[data2013$AnneeDiplome == 2011]), as.character(data2014$AnneeDiplome[data2014$AnneeDiplome == 2012]) ))
+
+situation2008_2012 = data.frame(situation=situationALL, promo=promoALL, poids=1, nb=1)
+situation2008_2012 = situation2008_2012[situation2008_2012$situation != "",]
+situation2008_2012 = situation2008_2012[! is.na(situation2008_2012$situation),]
+
+situation2008_2012$situation = relevel(situation2008_2012$situation, "En recherche d'emploi")
+situation2008_2012$situation = relevel(situation2008_2012$situation, "Volontariat")
+situation2008_2012$situation = relevel(situation2008_2012$situation, "En thèse")
+situation2008_2012$situation = relevel(situation2008_2012$situation, "En création d'entreprise")
+situation2008_2012$situation = relevel(situation2008_2012$situation, "En activité professionnelle")
+
+situation2008_2012[situation2008_2012$promo == 2008,"nb"] = length(situation2008_2012[situation2008_2012$promo == 2008,"situation"])
+situation2008_2012[situation2008_2012$promo == 2009,"nb"] = length(situation2008_2012[situation2008_2012$promo == 2009,"situation"])
+situation2008_2012[situation2008_2012$promo == 2010,"nb"] = length(situation2008_2012[situation2008_2012$promo == 2010,"situation"])
+situation2008_2012[situation2008_2012$promo == 2011,"nb"] = length(situation2008_2012[situation2008_2012$promo == 2011,"situation"])
+situation2008_2012[situation2008_2012$promo == 2012,"nb"] = length(situation2008_2012[situation2008_2012$promo == 2012,"situation"])
+situation2008_2012[,"poids"] = 100/situation2008_2012[,"nb"]
+
+
+ggplot(situation2008_2012, aes(x=promo,fill=situation, weight=poids)) + geom_bar() +scale_fill_manual(values=c("blue","green","SlateBlue4","chartreuse4","red","yellow", "orange")) + ylab("Pourcentage de réponses")
+ggsave("../../Output/ensimag_2008_2012_situation.png", width=2*par("din")[1])
+
+
+# promo 2009-2013 à 6 mois
+
+sit2009 = as.character(data2010_2009$situation.actuelle)
+sit2009[data2010_2009$Type.d.études == "Doctorat"] = "En thèse"
+sit2009[sit2009 == "A - Vous êtes en activité professionnelle"] = "En activité professionnelle"
+sit2009[sit2009 == "C - Vous êtes en Volontariat (VIE ou VIA)"] = "Volontariat"
+sit2009[sit2009 == "B - Vous êtes en recherche d'emploi"] = "En recherche d'emploi"
+sit2009[sit2009 == "D - Vous êtes en poursuite d'études"] = "En poursuite d'études (Hors thèse)"
+sit2009[sit2009 == "E - Vous êtes sans activité professionnelle volontairement sans emploi (ex : année sabbatique...)"] = "Sans activité volontairement"
+promo2009= sit2009
+promo2009[]= "2009"
+
+situationALL6= factor( gsub("Création d'entreprise (en projet)", "En création d'entreprise", fixed=T, c(as.character(sit2009), as.character(data2011$ActiviteActuelle[data2011$Promo == 2010]) , as.character(data2012$ActiviteActuelle[data2012$AnneeDiplome == 2011]), as.character(data2013$ActiviteActuelle[data2013$AnneeDiplome == 2012]), as.character(data2014$ActiviteActuelle[data2014$AnneeDiplome == 2013]) )))
+length(situationALL6)
+promoALL6= factor(c( as.character(promo2009), as.character(data2011$Promo[data2011$Promo == 2010]) , as.character(data2012$AnneeDiplome[data2012$AnneeDiplome == 2011]), as.character(data2013$AnneeDiplome[data2013$AnneeDiplome == 2012]), as.character(data2014$AnneeDiplome[data2014$AnneeDiplome == 2013]) ))
+
+situation2009_2013 = data.frame(situation=situationALL6, promo=promoALL6, poids=1, nb=1)
+
+situation2009_2013 = situation2009_2013[situation2009_2013$situation != "",]
+situation2009_2013 = situation2009_2013[! is.na(situation2009_2013$situation),]
+
+situation2009_2013$situation = relevel(situation2009_2013$situation, "En recherche d'emploi")
+situation2009_2013$situation = relevel(situation2009_2013$situation, "Volontariat")
+situation2009_2013$situation = relevel(situation2009_2013$situation, "En thèse")
+situation2009_2013$situation = relevel(situation2009_2013$situation, "En création d'entreprise")
+situation2009_2013$situation = relevel(situation2009_2013$situation, "En activité professionnelle")
+
+situation2009_2013[situation2009_2013$promo == 2008,"nb"] = length(situation2009_2013[situation2009_2013$promo == 2008,"situation"])
+situation2009_2013[situation2009_2013$promo == 2009,"nb"] = length(situation2009_2013[situation2009_2013$promo == 2009,"situation"])
+situation2009_2013[situation2009_2013$promo == 2010,"nb"] = length(situation2009_2013[situation2009_2013$promo == 2010,"situation"])
+situation2009_2013[situation2009_2013$promo == 2011,"nb"] = length(situation2009_2013[situation2009_2013$promo == 2011,"situation"])
+situation2009_2013[situation2009_2013$promo == 2012,"nb"] = length(situation2009_2013[situation2009_2013$promo == 2012,"situation"])
+situation2009_2013[situation2009_2013$promo == 2013,"nb"] = length(situation2009_2013[situation2009_2013$promo == 2013,"situation"])
+situation2009_2013[,"poids"] = 100/situation2009_2013[,"nb"]
+
+
+ggplot(situation2009_2013, aes(x=promo,fill=situation, weight=poids)) + geom_bar() +scale_fill_manual(values=c("blue","green","SlateBlue4","chartreuse4","red","yellow", "orange")) + ylab("Pourcentage de réponses")
+ggsave("../../Output/ensimag_2009_2013_situation.png", width=2*par("din")[1])
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
