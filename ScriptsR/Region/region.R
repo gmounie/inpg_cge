0b20fce3ce813c8aa89e835605f501303c8dff16
Grégory Mounié <Gregory.Mounie@imag.fr>
2014-04-15 17:06:21 +0200

    les regions

--- ScriptsR/Region/region.R
+++ ScriptsR/Region/region.R
@@ -7,47 +7,40 @@ library(plyr)
 ## Durée de la recherche d'emploi
 # l'info n'est pas obligatoire pour les 2014, peu remplit !
 
-r14 = data.frame(region = data2014$LieuTravailDetailEnquete2012, labo= data2014$LieuLaboDoctoratEnquete2012, promo=  data2014$AnneeDiplome)
-r13 = data.frame(region = data2013$LieuTravailDetailEnquete2012, labo= data2013$LieuLaboDoctoratEnquete2012, promo=  data2013$AnneeDiplome)
-r12 = data.frame(region = data2012$LieuTravailDetailEnquete2012, labo= data2012$LieuLaboDoctoratEnquete2012, promo=  data2012$AnneeDiplome)
+r14 = data.frame(lieu = data2014$LieuTravailDetailEnquete2012, labo= data2014$LieuLaboDoctoratEnquete2012, promo=  data2014$AnneeDiplome)
+r13 = data.frame(lieu = data2013$LieuTravailDetailEnquete2012, labo= data2013$LieuLaboDoctoratEnquete2012, promo=  data2013$AnneeDiplome)
+r12 = data.frame(lieu = data2012$LieuTravailDetailEnquete2012, labo= data2012$LieuLaboDoctoratEnquete2012, promo=  data2012$AnneeDiplome)
 
 r = rbind(r14[r14$promo == 2012,], r13[r13$promo == 2011,] ,r12[r12$promo == 2010,])
-r$region = factor(r$region)
-r= r[r$region != "" || r$labo != "",]
+r$region = paste(as.character(r$lieu),as.character(r$labo),sep="")
 
+
+r$region = factor(r$region)
+r$promo = factor(r$promo)
+r= r[r$lieu != "" || r$labo != "",]
+r= r[r$promo != "NA",]
 
 length(r$region)
 summary(r)
 
-r14 = data.frame(contrat = as.character(data2014$ContratTravailActuelV2014), enquete=2014)
-r13 = data.frame(contrat = as.character(data2013$ContratTravailActuelEnquete2012), enquete=2013)
-r12 = data.frame(contrat = as.character(data2012$ContratTravailActuelEnquete2012), enquete=2012)
-r11 = data.frame(contrat = as.character(data2011$ContratTravailActuel), enquete=2011)
-r10 = data.frame(contrat = c(as.character(data2010_2009$emp.actuel.contrat), as.character(data2010_2008$emp.actuel.contrat)), enquete=2010)
 
-# pas d'information dans l'enquete 2014
-r = rbind(r14, r13,r12,r11,r10)
-r$enquete = factor(r$enquete)
-r= r[r$contrat != "",]
-#levels(r$durée)
-#levels(r$durée) = c("","6 mois ou plus","Contrat avant la sortie de l'école","De 2 à moins de 4 mois","De 4 à moins de 6 mois", "Moins de 2 mois de recherche",  "Contrat avant la sortie de l'école", "Moins de 2 mois de recherche", "Moins de 2 mois de recherche" )
-
-## r$durée = relevel(r$durée, "6 mois ou plus")
-## r$durée = relevel(r$durée, "De 4 à moins de 6 mois")
-## r$durée = relevel(r$durée, "De 2 à moins de 4 mois")
-## r$durée = relevel(r$durée, "Moins de 2 mois de recherche")
-## r$durée = relevel(r$durée, "Contrat avant la sortie de l'école")
-
-
-length(r$contrat)
-summary(r)
-qplot(r)
-ggplot(r, aes(x=enquete, fill=contrat)) + geom_bar() + xlab("Année enquête") + ylab("Nombre de diplômés")
+ggplot(r, aes(x=promo, fill=region)) + geom_bar() + xlab("Année enquête") + ylab("Nombre de diplômés")
 ggsave("../../Output/ensimag_duree_recherche.png", width=2*par("din")[1])
 
 
 ## avec la moyenne et la médiane ? boxplot ?
 
+r6 = rbind(r14[r14$promo == 2013,], r13[r13$promo == 2012,] ,r12[r12$promo == 2011,])
+r6$region = paste(as.character(r6$lieu),as.character(r6$labo),sep="")
+
+
+r6$region = factor(r6$region)
+r6$promo = factor(r6$promo)
+r6= r6[r6$lieu != "" || r6$labo != "",]
+r6= r6[r6$promo != "NA",]
+
+length(r6$region)
+summary(r6)
 
 
 
