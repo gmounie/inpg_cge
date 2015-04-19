2984462b02c1e2f900a9509ecca328d74826f128
Grégory Mounié <Gregory.Mounie@imag.fr>
2015-04-19 11:55:22 +0200

    lieu 2015

--- ScriptsR/Lieux/lieux.R
+++ ScriptsR/Lieux/lieux.R
@@ -2,6 +2,22 @@ source("../DataReader/dataReader.R")
 
 library(ggplot2)
 
+                                        # 2015
+                                        # Très basique avec juste RA, PACA et Province pour le reste
+lieu = data.frame( lieutrav = data2015$LieuTravailRegionEtrangerV2015 ,lieuvol = data2015$LieuVolontariatRegion, lieudoc = data2015$LieuLaboDoctoratEnquete2012)
+lieu$lieu[lieu$lieutrav != ""] = as.character(lieu$lieutrav[lieu$lieutrav != ""])
+lieu$lieu[lieu$lieudoc != ""] = as.character(lieu$lieudoc[lieu$lieudoc != ""])
+lieu$lieu[lieu$lieuvol != ""] = as.character(lieu$lieuvol[lieu$lieuvol != ""])
+lieu$lieu = factor(lieu$lieu)
+a = data.frame(promo = data2015$Promo, filiere=data2015$Option_FiliereFormation, lieu = lieu$lieu, weight=1/length(data2015$Promo))
+#level(a$lieu) = c("Aquitaine", "Auvergne", "Bretagne", "Champagne-Ardenne", "Etranger", "Franche-Comté", "Île-de-France", "Languedoc-Roussillon", "La Réunion", "Lorraine", "Midi-Pyrénées", "Nord-Pas-de-Calais", "Pays de la Loire", "Provence-Alpes-Côte d'Azur", "Rhône-Alpes")
+levels(a$lieu) = c("Province-Sud", "Rhône-Alpes-Auvergne", "Province-Nord", "Province-Nord", "Etranger", "Province-Nord", "Île-de-France", "Province-Sud", "DOM/TOM", "Province-Nord", "Province-Sud", "Province-Nord", "Province-Nord", "Provence-Alpes-Côte d'Azur", "Rhône-Alpes-Auvergne")
+p = ggplot(data=a, aes(x=lieu, fill=filiere))  + geom_bar((aes(weight=weight))) + coord_flip()
+p = p  + theme(plot.title=element_text("Lieu de travail (doctorat, volontariat) en fonction de la filière")) + xlab("Lieu") + scale_fill_hue(l=70, c=150)  + ylab("Nb de diplômés")
+p
+ggsave("../../Output/ensimag_2015_lieu.png", width=2*par("din")[1])
+
+
 for (i in 0:2) { 
   if (i == 0) {
     data = data2013
@@ -21,8 +37,8 @@ for (i in 0:2) {
     lapromo = data$PromoEnquete2012
     lafiliere= data$FiliereFormation
     anneeE = 2012
-} else {
-    data = data2014
+} else if (i == 2){
+-    data = data2014
     data = data[data$Promo == 2013 | data$Promo == 2012,]
     fname = "../../Output/ensimag_2014_lieu.pdf"
     fnameF = "../../Output/ensimag_2014_lieu_filiere.pdf"
@@ -31,7 +47,16 @@ for (i in 0:2) {
     lafiliere= data$FiliereFormation
     anneeE = 2014
     
-  }
+} else if (i == 3) {
+    data = data2015
+    fname = "../../Output/ensimag_2015_lieu.pdf"
+    fnameF = "../../Output/ensimag_2015_lieu_filiere.pdf"
+    titre = "Lieu de travail des diplômés, par type d'activité, début 2O15, 6 et 18 mois"
+    lapromo = data$Promo
+    lafiliere= data$Option_FiliereFormation
+    anneeE = 2015
+}
+}
 
 
   lieu = data.frame(promo=as.factor(lapromo), lieudoc = data$LieuLaboDoctoratEnquete2012, lieuTravail = data$LieuTravailDetailEnquete2012, activites= data$ActiviteActuelle, anneeEnquete = data$AnneeEnquete, filiere=lafiliere)
