471435b4a43644ef123af90091a115952d9d5c31
Grégory Mounié <Gregory.Mounie@imag.fr>
2016-04-03 21:24:41 +0200

    changement dans la gestion de la filière
    regroupement des master

--- ScriptsR/Repondants/repondants_par_filiere.R
+++ ScriptsR/Repondants/repondants_par_filiere.R
@@ -1,10 +1,11 @@
 source("../DataReader/dataReader.R")
 library(ggplot2)
 a = data.frame(Filiere=data2016$Option_FiliereFormation, FSN= data2016$Option_FiliereFormation)
-levels(a$FSN) = c(1:12)
-p = ggplot(a, aes(x=FSN,fill=Filiere)) + geom_bar(aes(show_guide=FALSE))+coord_flip()+ geom_text(label="FILIÈRE FINANCE décalé de 1 an (sous-représenté)!!!", color="red", x=3, y=100) + xlab("") + ylab("")
+levels(a$Filiere) = c( "Master" , "Master" , "IF" , "ISI" , "Tcom/ISSC" , "Master" , "Master" , "Master" , "MMIS" , "SLE" , "Tcom/ISSC")
+levels(a$FSN) = c(1,1,2,3,4,1,1,1,5,6,4)
+p = ggplot(a, aes(x=FSN,fill=Filiere)) + geom_bar(colour="white",aes(show_guide=FALSE))+coord_flip()+ geom_text(label="FILIÈRE FINANCE décalé de 1 an (sous-représenté)!!!", color="red", x=2, y=100) + xlab("") + ylab("")
 p
-ggsave("../../Output/ensimag_2016_repondants_filiere.png", width=1.2*par("din")[1])
+ggsave("../../Output/ensimag_2016_repondants_filiere.png", width=2*par("din")[1])
 
 
 # 2015
@@ -13,3 +14,11 @@ levels(a$FSN) = c(1:10)
 p = ggplot(a, aes(x=FSN,fill=Filiere)) + geom_bar(aes(show_guide=FALSE))+coord_flip()+ geom_text(label="FILIÈRE FINANCE décalé de 6 MOIS (sous-représenté: 2/3)!!!", color="red", x=4, y=100) + xlab("") + ylab("")
 p
 ggsave("../../Output/ensimag_2015_repondants_filiere.png", width=1.2*par("din")[1])
+
+
+
+
+
+
+
+
