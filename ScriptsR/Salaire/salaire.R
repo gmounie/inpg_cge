99b143ec81fcb28cbc0d8faf4b914e9650011b2d
Grégory Mounié <Gregory.Mounie@imag.fr>
2017-08-27 18:34:59 +0200

    ajout du calcul de salire identique à la CGE

--- ScriptsR/Salaire/salaire.R
+++ ScriptsR/Salaire/salaire.R
@@ -14,6 +14,11 @@ salaireBrut2014 = as.data.frame(data2014[ data2014$AnneeEnquete == 2014, c("Sala
 salaireBrut2013 = as.data.frame(data2013[ data2013$AnneeEnquete == 2013, c("SalaireBrutAnnuelAvecPrimes", "SalaireBrutAnnuelHorsPrimes", "Sexe", "AnneeDiplome", "AnneeEnquete", "RevenuBrutAnnuelAvecPrimesThesard", "RevenuBrutAnnuelHorsPrimesThesard", "ActiviteActuelle", "LieuTravailDetailEnquete2012", "LieuLaboDoctoratEnquete2012")])
 salaireBrut2012 = as.data.frame(data2012[ data2012$AnneeEnquete == 2012, c("SalaireBrutAnnuelAvecPrimes", "SalaireBrutAnnuelHorsPrimes", "Sexe", "AnneeDiplome", "AnneeEnquete", "RevenuBrutAnnuelAvecPrimesThesard", "RevenuBrutAnnuelHorsPrimesThesard", "ActiviteActuelle", "LieuTravailDetailEnquete2012", "LieuLaboDoctoratEnquete2012")])
 
+## equiv CGE
+
+
+
+
 salaire = rbind(salaireBrut2017, salaireBrut2016, salaireBrut2015, salaireBrut2014, salaireBrut2013, salaireBrut2012)
 salaire = salaire[salaire$Sexe != "" & ! is.na(salaire$Sexe),]
 salaire = salaire[salaire$AnneeDiplome != "" & ! is.na(salaire$AnneeDiplome),]
@@ -42,9 +47,41 @@ salaire[salaire$lieu == "","lieu"] = "Non renseigné"
 
 library(ggplot2)
 
+## CGE: juste Industrie
+                                        # France sans prime
+summary(salaireBrut2017$SalaireBrutAnnuelAvecPrimes[salaireBrut2017$AnneeDiplome== 2016 & salaireBrut2017$LieuTravailDetailEnquete2012 == "Abroad"])
+summary(salaireBrut2017$SalaireBrutAnnuelAvecPrimes[salaireBrut2017$AnneeDiplome== 2016 & salaireBrut2017$LieuTravailDetailEnquete2012 != "Abroad"])
+
+summary(salaireBrut2017$SalaireBrutAnnuelAvecPrimes[salaireBrut2017$AnneeDiplome== 2015 & salaireBrut2017$LieuTravailDetailEnquete2012 == "Abroad"])
+summary(salaireBrut2017$SalaireBrutAnnuelAvecPrimes[salaireBrut2017$AnneeDiplome== 2015 & salaireBrut2017$LieuTravailDetailEnquete2012 != "Abroad"])
+
+summary(salaireBrut2017$SalaireBrutAnnuelAvecPrimes[salaireBrut2017$AnneeDiplome== 2014 & salaireBrut2017$LieuTravailDetailEnquete2012 == "Abroad"])
+summary(salaireBrut2017$SalaireBrutAnnuelAvecPrimes[salaireBrut2017$AnneeDiplome== 2014 & salaireBrut2017$LieuTravailDetailEnquete2012 != "Abroad"])
+
+summary(salaireBrut2017$SalaireBrutAnnuelAvecPrimes[salaireBrut2017$AnneeDiplome== 2016 & salaireBrut2017$LieuTravailDetailEnquete2012 == "Abroad" & salaireBrut2017$Sexe == "Homme"])
+summary(salaireBrut2017$SalaireBrutAnnuelAvecPrimes[salaireBrut2017$AnneeDiplome== 2016 & salaireBrut2017$LieuTravailDetailEnquete2012 == "Abroad" & salaireBrut2017$Sexe == "Femme"])
+
+summary(salaireBrut2017$SalaireBrutAnnuelAvecPrimes[salaireBrut2017$AnneeDiplome== 2015 & salaireBrut2017$LieuTravailDetailEnquete2012 == "Abroad" & salaireBrut2017$Sexe == "Homme"])
+summary(salaireBrut2017$SalaireBrutAnnuelAvecPrimes[salaireBrut2017$AnneeDiplome== 2015 & salaireBrut2017$LieuTravailDetailEnquete2012 == "Abroad" & salaireBrut2017$Sexe == "Femme"])
+
+summary(salaireBrut2017$SalaireBrutAnnuelAvecPrimes[salaireBrut2017$AnneeDiplome== 2014 & salaireBrut2017$LieuTravailDetailEnquete2012 == "Abroad" & salaireBrut2017$Sexe == "Homme"])
+summary(salaireBrut2017$SalaireBrutAnnuelAvecPrimes[salaireBrut2017$AnneeDiplome== 2014 & salaireBrut2017$LieuTravailDetailEnquete2012 == "Abroad" & salaireBrut2017$Sexe == "Femme"])
+
+
+                                        # France avec prime
+
+                                        # par sexe avec prime
+
+
+
 # par sexe avec prime
 summary(salaire$SalaireAvecPrime[salaire$AnneeEnquete == 2017 & salaire$AnneeDiplome== 2016 & salaire$Sexe == "Femme"])
 summary(salaire$SalaireAvecPrime[salaire$AnneeEnquete == 2017 & salaire$AnneeDiplome== 2016 & salaire$Sexe == "Homme"])
+summary(salaire$SalaireAvecPrime[salaire$AnneeEnquete == 2017 & salaire$AnneeDiplome== 2015 & salaire$Sexe == "Femme"])
+summary(salaire$SalaireAvecPrime[salaire$AnneeEnquete == 2017 & salaire$AnneeDiplome== 2015 & salaire$Sexe == "Homme"])
+summary(salaire$SalaireAvecPrime[salaire$AnneeEnquete == 2017 & salaire$AnneeDiplome== 2014 & salaire$Sexe == "Femme"])
+summary(salaire$SalaireAvecPrime[salaire$AnneeEnquete == 2017 & salaire$AnneeDiplome== 2014 & salaire$Sexe == "Homme"])
+
 summary(salaire$SalaireAvecPrime[salaire$AnneeEnquete == 2016 & salaire$AnneeDiplome== 2015 & salaire$Sexe == "Femme"])
 summary(salaire$SalaireAvecPrime[salaire$AnneeEnquete == 2016 & salaire$AnneeDiplome== 2015 & salaire$Sexe == "Homme"])
 summary(salaire$SalaireAvecPrime[salaire$AnneeEnquete == 2015 & salaire$AnneeDiplome== 2014 & salaire$Sexe == "Femme"])
@@ -56,17 +93,17 @@ summary(salaire$SalaireAvecPrime[salaire$AnneeEnquete == 2013 & salaire$AnneeDip
 
 # par sexe sans prime
 summary(salaire$SalaireHorsPrime[salaire$AnneeEnquete == 2017 & salaire$AnneeDiplome== 2016 & salaire$Sexe == "Femme"])
-summary(salaire$SalaireAvecPrime[salaire$AnneeEnquete == 2017 & salaire$AnneeDiplome== 2016 & salaire$Sexe == "Homme"])
+summary(salaire$SalaireHorsPrime[salaire$AnneeEnquete == 2017 & salaire$AnneeDiplome== 2016 & salaire$Sexe == "Homme"])
 summary(salaire$SalaireHorsPrime[salaire$AnneeEnquete == 2016 & salaire$AnneeDiplome== 2015 & salaire$Sexe == "Femme"])
-summary(salaire$SalaireAvecPrime[salaire$AnneeEnquete == 2016 & salaire$AnneeDiplome== 2015 & salaire$Sexe == "Homme"])
-summary(salaire$SalaireAvecPrime[salaire$AnneeEnquete == 2015 & salaire$AnneeDiplome== 2014 & salaire$Sexe == "Femme"])
-summary(salaire$SalaireAvecPrime[salaire$AnneeEnquete == 2015 & salaire$AnneeDiplome== 2014 & salaire$Sexe == "Homme"])
-summary(salaire$SalaireAvecPrime[salaire$AnneeEnquete == 2014 & salaire$AnneeDiplome== 2013 & salaire$Sexe == "Femme"])
-summary(salaire$SalaireAvecPrime[salaire$AnneeEnquete == 2014 & salaire$AnneeDiplome== 2013 & salaire$Sexe == "Homme"])
-summary(salaire$SalaireAvecPrime[salaire$AnneeEnquete == 2013 & salaire$AnneeDiplome== 2012 & salaire$Sexe == "Femme"])
-summary(salaire$SalaireAvecPrime[salaire$AnneeEnquete == 2013 & salaire$AnneeDiplome== 2012 & salaire$Sexe == "Homme"])
+summary(salaire$SalaireHorsPrime[salaire$AnneeEnquete == 2016 & salaire$AnneeDiplome== 2015 & salaire$Sexe == "Homme"])
+summary(salaire$SalaireHorsPrime[salaire$AnneeEnquete == 2015 & salaire$AnneeDiplome== 2014 & salaire$Sexe == "Femme"])
+summary(salaire$SalaireHorsPrime[salaire$AnneeEnquete == 2015 & salaire$AnneeDiplome== 2014 & salaire$Sexe == "Homme"])
+summary(salaire$SalaireHorsPrime[salaire$AnneeEnquete == 2014 & salaire$AnneeDiplome== 2013 & salaire$Sexe == "Femme"])
+summary(salaire$SalaireHorsPrime[salaire$AnneeEnquete == 2014 & salaire$AnneeDiplome== 2013 & salaire$Sexe == "Homme"])
+summary(salaire$SalaireHorsPrime[salaire$AnneeEnquete == 2013 & salaire$AnneeDiplome== 2012 & salaire$Sexe == "Femme"])
+summary(salaire$SalaireHorsPrime[salaire$AnneeEnquete == 2013 & salaire$AnneeDiplome== 2012 & salaire$Sexe == "Homme"])
 
-                                        # hors prime
+                                        # avec prime
 summary(salaire$SalaireAvecPrime[salaire$AnneeEnquete == 2017 & salaire$AnneeDiplome== 2016 & salaire$Sexe == "Femme"])
 summary(salaire$SalaireAvecPrime[salaire$AnneeEnquete == 2017 & salaire$AnneeDiplome== 2016 & salaire$Sexe == "Homme"])
 summary(salaire$SalaireAvecPrime[salaire$AnneeEnquete == 2016 & salaire$AnneeDiplome== 2015 & salaire$Sexe == "Femme"])
@@ -161,9 +198,9 @@ ggsave("../../Output/ensimag_2017_salaire_total_inf100000.png", width=1.5*par("d
 # salaire 2017, France, hors thèse
 
 s17 = data.frame(AnneeDiplome= factor(data2017$X21..AnneeDiplomeVerifieParLeDiplome), SalaireAvecPrime = data2017$X191..Emploi_SalaireBrutAnnuelAvecPrimes, lieu=data2017$X38..EmploiLieuRegionEtranger, activite= data2017$X26..ActiviteActuelle, filiere=data2017$X247..Option_ScolariteFiliereFormation)
-s17f = na.omit(s17[s17$lieu != "Abroad" & s17$activite != "Studying for a PhD" & s17$SalaireAvecPrime != 0,])
+s17f = na.omit(s17[s17$SalaireAvecPrime < 100000 & s17$lieu != "Abroad" & s17$activite != "Studying for a PhD" & s17$SalaireAvecPrime != 0,])
 p = ggplot(data=s17f, aes(x=AnneeDiplome, y=SalaireAvecPrime, ymin=0)) + geom_jitter(position = position_jitter(width=0.3), aes(alpha=0.01)) + geom_boxplot(outlier.size=0.1)
-p + theme(title=element_text("Salaire Brut (+ primes) France, hors thèse, par année de promotion et année de l'enquête")) + ggtitle("Salaire France, hors thèse, avec prime, enquête 2017")
+p + theme(title=element_text("Salaire Brut (+ primes) France, hors thèse, par année de promotion et année de l'enquête")) + geom_text(label="2 2015 > 100kE", x= 2, y = 85000 , color = "red", size= 8) + ggtitle("Salaire France, hors thèse, avec prime, enquête 2017")
 ggsave("../../Output/ensimag_2017_salaire_france_industrie.png", width=1.5*par("din")[1])
 
 na.omit(s17[s17$lieu != "Abroad" & s17$activite != "Studying for a PhD" & s17$SalaireAvecPrime > 60000,])$lieu
