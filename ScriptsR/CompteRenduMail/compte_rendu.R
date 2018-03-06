d73cc0586a146ac329cbeb5c8c1f20056d6da17a
Grégory Mounié <Gregory.Mounie@imag.fr>
2018-03-06 18:04:32 +0100

    cpt en R

---
+++ ScriptsR/CompteRenduMail/compte_rendu.R
@@ -0,0 +1,52 @@
+source("../DataReader/dataReader.R")
+# enquete 2018
+repondants6_2017 = length( data2018$X244..AnneeEnquete[ data2018$X244..AnneeEnquete == 2018 & data2018$X14..AnneeDiplomeVerifieParLeDiplome == 2017 & data2018$X20..ActiviteActuelle != ""])
+repondants18_2016 = length( data2018$X244..AnneeEnquete[ data2018$X244..AnneeEnquete == 2018 & data2018$X14..AnneeDiplomeVerifieParLeDiplome == 2016 & data2018$X20..ActiviteActuelle != ""])
+repondants30_2015 = length( data2018$X244..AnneeEnquete[ data2018$X244..AnneeEnquete == 2018 & data2018$X14..AnneeDiplomeVerifieParLeDiplome == 2015 & data2018$X20..ActiviteActuelle != ""])
+
+sink(file="sortie2018.txt", append=FALSE, split=TRUE)
+
+print("### % Répondants par promo 2017, 2016 et 2015 ###")
+print(repondants6_2017/taille2017)
+print(repondants18_2016/taille2016)
+print(repondants30_2015/taille2015)
+
+
+postes = data.frame(poste=data2018$X256..Option_EmploiPosteListe, activité=data2018$X20..ActiviteActuelle, secteur=data2018$X55..EmploiEntrepriseSecteurActivite, lieu=data2018$X33..EmploiLieuRegionEtranger, lieuThese=data2018$X122..TheseRegionEtranger, salaire=data2018$X186..Emploi_SalaireBrutAnnuelAvecPrimes, salaireThese=data2018$X136..TheseSalaireBrutAnnuelAvecPrimes)
+
+
+
+                                        # postes
+print("### Situation  (1er chiffre: NA non répondu)###")
+table(postes$activité)
+
+print("### Postes occupés (1er chiffre: NA non répondu ou pas concerné)###")
+table(postes$poste)
+
+levels(postes$secteur) = c(levels(postes$secteur), "Recherche (doctorat)")
+postes[postes$activité == "En thèse",]$secteur = "Recherche (doctorat)"
+print("### Secteur (1er chiffre: NA non répondu ou pas concerné)###")
+table(postes$secteur)
+
+print("### Lieu (1er chiffre: NA non répondu ou pas concerné)###")
+table(postes$lieu)
+
+print("### Lieu doctorat (1er chiffre: NA non répondu ou pas concerné)###")
+table(postes$lieuThese)
+
+print("### Salaire France (PAS NETTOYÉ: MIN, MEAN, MAX BIDON) ###")
+quantile(postes$salaire[postes$lieu != "Étranger"], na.rm=TRUE)
+
+print("### Salaire Étranger (PAS NETTOYÉ: MIN et MAX BIDON) ###")
+quantile(postes$salaire[postes$lieu == "Étranger"], na.rm=TRUE)
+
+print("### Salaire Doctorat France (PAS NETTOYÉ: MIN, MEAN, MAX BIDON) ###")
+quantile(postes$salaireThese[postes$lieuThese != "Étranger"], na.rm=TRUE)
+
+print("### Salaire Étranger (PAS NETTOYÉ: MIN et MAX BIDON) ###")
+quantile(postes$salaireThese[postes$lieuThese == "Étranger"], na.rm=TRUE)
+
+sink()
+
+
+
