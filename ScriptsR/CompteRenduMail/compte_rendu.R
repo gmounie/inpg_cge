d7fb6345a74e2f6f04d16cdbc598d9dfa4dbf118
Grégory Mounié <Gregory.Mounie@imag.fr>
2018-03-13 16:15:42 +0100

    utilise summary sur data frame

--- ScriptsR/CompteRenduMail/compte_rendu.R
+++ ScriptsR/CompteRenduMail/compte_rendu.R
@@ -12,39 +12,40 @@ print(repondants18_2016/taille2016)
 print(repondants30_2015/taille2015)
 
 
-postes = data.frame(poste=data2018$X256..Option_EmploiPosteListe, activité=data2018$X20..ActiviteActuelle, secteur=data2018$X55..EmploiEntrepriseSecteurActivite, lieu=data2018$X33..EmploiLieuRegionEtranger, lieuThese=data2018$X122..TheseRegionEtranger, salaire=data2018$X186..Emploi_SalaireBrutAnnuelAvecPrimes, salaireThese=data2018$X136..TheseSalaireBrutAnnuelAvecPrimes)
+postes = data.frame(poste=data2018$X256..Option_EmploiPosteListe, activité=data2018$X20..ActiviteActuelle, secteur=data2018$X55..EmploiEntrepriseSecteurActivite, lieu=data2018$X33..EmploiLieuRegionEtranger, lieuThese=data2018$X122..TheseRegionEtranger, salaire=data2018$X183..EmploiCorrige_BrutAnnuelHorsPrimes, salaireThese=data2018$X136..TheseSalaireBrutAnnuelAvecPrimes)
 
 
 
                                         # postes
-print("### Situation  (1er chiffre: NA non répondu)###")
-table(postes$activité)
+print("### Situation, les 3 promos  (Blanc: NA non répondu)###")
+summary(as.data.frame(postes$activité), maxsum=10)
 
-print("### Postes occupés (1er chiffre: NA non répondu ou pas concerné)###")
-table(postes$poste)
+print("### Postes occupés , les 3 promos  (Blanc: NA non répondu ou pas concerné)###")
+summary(as.data.frame(postes$poste), maxsum=10)
+print("### Nombre de type de postes, les 3 promo ###")
+length(levels(postes$poste))
 
 levels(postes$secteur) = c(levels(postes$secteur), "Recherche (doctorat)")
 postes[postes$activité == "En thèse",]$secteur = "Recherche (doctorat)"
 print("### Secteur (1er chiffre: NA non répondu ou pas concerné)###")
-table(postes$secteur)
+summary(as.data.frame(postes$secteur), maxsum=10)
+print("### Nombre de type de secteurs, les 3 promo ###")
+length(levels(postes$secteur))
 
-print("### Lieu (1er chiffre: NA non répondu ou pas concerné)###")
-table(postes$lieu)
+print("### Lieu Entreprise (1er chiffre: NA non répondu ou pas concerné)###")
+summary(as.data.frame(postes$lieu), maxsum=10)
 
-print("### Lieu doctorat (1er chiffre: NA non répondu ou pas concerné)###")
-table(postes$lieuThese)
+print("### Lieu Doctorat (1er chiffre: NA non répondu ou pas concerné)###")
+summary(as.data.frame(postes$lieuThese), maxsum=10)
 
-print("### Salaire France (PAS NETTOYÉ: MIN, MEAN, MAX BIDON) ###")
-quantile(postes$salaire[postes$lieu != "Étranger"], na.rm=TRUE)
+print("### Quartile Salaire France  ###")
+quantile(postes$salaire[postes$lieu != "Étranger"], na.rm=TRUE, probs = seq(0.25,0.75, 0.25))
 
-print("### Salaire Étranger (PAS NETTOYÉ: MIN et MAX BIDON) ###")
-quantile(postes$salaire[postes$lieu == "Étranger"], na.rm=TRUE)
+print("### Quartile Salaire Étranger ###")
+quantile(postes$salaire[postes$lieu == "Étranger"], na.rm=TRUE, probs = seq(0.25,0.75, 0.25))
 
-print("### Salaire Doctorat France (PAS NETTOYÉ: MIN, MEAN, MAX BIDON) ###")
-quantile(postes$salaireThese[postes$lieuThese != "Étranger"], na.rm=TRUE)
-
-print("### Salaire Étranger (PAS NETTOYÉ: MIN et MAX BIDON) ###")
-quantile(postes$salaireThese[postes$lieuThese == "Étranger"], na.rm=TRUE)
+print("### Quartile Salaire Doctorat France et Étranger ###")
+quantile(postes$salaireThese, na.rm=TRUE, probs = seq(0.25,0.75, 0.25))
 
 sink()
 
